f08e1d7d659b0d452566432201ef5bdb6ccb8492
rIYA3332 <riyabhatta123@gmail.com>
2025-12-07 14:53:03 +0545

    module entity added

--- src/main/java/com/cms/coursemanagementsystem/dto/request/student/CreateStudentDTO.java
+++ src/main/java/com/cms/coursemanagementsystem/dto/request/student/CreateStudentDTO.java
@@ -3,6 +3,5 @@ package com.cms.coursemanagementsystem.dto.request.student;
 public record CreateStudentDTO(
         String fullName,
         String email
-        // Enrollment will be handled in a separate method/controller,
-        // or through a list of course IDs if you prefer. Sticking to simple for now.
+        // Enrollment will be handled in a separate method/controller
 ) {}
